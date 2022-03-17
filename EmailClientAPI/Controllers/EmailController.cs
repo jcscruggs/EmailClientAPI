40ab34f26430e2e4c6a8d7921b2f5ab1fab8e4cd
jcscruggs <jaredscruggs@outlook.com>
2022-03-16 21:35:03 -0400

    Add project files.

---
+++ EmailClientAPI/Controllers/EmailController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmailLibrary;
+
+namespace EmailClientAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+
+        private readonly ILogger<EmailController> _logger;
+        private readonly IEmailClient _emailClient;
+        public EmailController(IEmailClient emailClient, ILogger<EmailController> logger)
+        {
+            _logger = logger;
+            _emailClient = emailClient;
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _emailClient.connect();
+            _logger.LogInformation("entered get method");
+            return Ok("Completed");
+        }
+
+
+    }
+}
